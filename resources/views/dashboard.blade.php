dcc7b75d9329db6a128395fb4f81c4021ab14cf1
Chamika Yapa <chamikayapa@Chamikas-MacBook-Pro.local>
2024-09-19 19:49:08 +1000

    message4

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,7 +12,6 @@
                     <div class="p-6 text-gray-900">
                         {{-- {{ __("You're logged in!") }} --}}
                         Access Management Portals
-                        
                     </div>
                 </div>
             </a>
